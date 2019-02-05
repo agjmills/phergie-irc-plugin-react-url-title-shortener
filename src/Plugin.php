eaf42dc555658a732e7b9514ccd6ec72460d2ea8
Alex Mills <alex@asdfx.us>
2019-02-05 20:33:40 +0000

    wrap in isset and fix indentation

--- src/Plugin.php
+++ src/Plugin.php
@@ -7,7 +7,8 @@ use Phergie\Irc\Bot\React\EventQueueInterface;
 use Phergie\Irc\Event\UserEventInterface;
 use Zttp\Zttp;
 
-class Plugin extends AbstractPlugin {
+class Plugin extends AbstractPlugin
+{
 
     public function getSubscribedEvents()
     {
@@ -17,9 +18,9 @@ class Plugin extends AbstractPlugin {
     public function handleCommand(UserEventInterface $event, EventQueueInterface $queue)
     {
         $eventParams = $event->getParams();
-        $message_parts = explode(' ' , $eventParams['text']);
+        $message_parts = explode(' ', $eventParams['text']);
         foreach ($message_parts as $message_part) {
-            if (strpos($message_part, 'http://') === 0 || strpos($message_part, 'https://') ===0) {
+            if (strpos($message_part, 'http://') === 0 || strpos($message_part, 'https://') === 0) {
                 $response = Zttp::get($message_part);
                 $body = $response->body();
                 preg_match("/<title>(.*?)<\/title>/", $body, $matches);
@@ -27,30 +28,29 @@ class Plugin extends AbstractPlugin {
                 if (count($matches) > 1) {
                     $title = $matches[1];
                 }
-            }
 
-            $shortUrl = $this->shorten($message_part);
-            
-            if ($title && $shortUrl) {
-               $queue->ircPrivmsg($event->getSource(), sprintf('^ %s → %s', $title, $shortUrl)); 
-               return;
-            }
+                $shortUrl = $this->shorten($message_part);
 
-            if ($title) {
-               $queue->ircPrivmsg($event->getSource(), sprintf('^ %s', $title)); 
-               return;
-            }
+                if (isset($title) && isset($shortUrl)) {
+                    $queue->ircPrivmsg($event->getSource(), sprintf('^ %s → %s', $title, $shortUrl));
+                    return;
+                }
 
+                if (isset($title)) {
+                    $queue->ircPrivmsg($event->getSource(), sprintf('^ %s', $title));
+                    return;
+                }
 
-            if ($shortUrl) {
-               $queue->ircPrivmsg($event->getSource(), sprintf('→ %s', $shortUrl)); 
-               return;
+                if (isset($shortUrl)) {
+                    $queue->ircPrivmsg($event->getSource(), sprintf('→ %s', $shortUrl));
+                    return;
+                }
             }
         }
     }
 
 
-    protected function shorten($url) 
+    protected function shorten($url)
     {
         $response = Zttp::get(sprintf('https://is.gd/create.php?format=simple&url=%s', $url));
         if ($response->status() === 200) {
